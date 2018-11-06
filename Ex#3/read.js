18eaaf77371de59ea690404f830781e96326cbe4
OlgaLyakhvatskaya <belsquirrel213@gmail.com>
2018-11-06 12:34:23 +0200

    fixed without return

--- Ex#3/read.js
+++ Ex#3/read.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 
-const read = (name) =>  {
-    return new Promise( (res, rej) => {
-        fs.readFile(name, (err, data) => {
-            if (err) return rej(err)
-            res(data.toString('utf8'))
-        })
-    })
-};
+const read = (name) =>  new Promise( (res, rej) => {
+    fs.readFile(name, (err, data) => {
+        if (err) return rej(err)
+        res(data.toString('utf8'))
+     })
+})
 
 
 module.exports = read;
