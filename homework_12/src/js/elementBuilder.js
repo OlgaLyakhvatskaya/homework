108119869de937ab0efbf7a3da0aa8cfc013f567
dmgame <denis.m.pcspace@gmail.com>
2018-11-27 19:07:35 +0200

    base

--- homework_12/src/js/elementBuilder.js
+++
@@ -1,6 +0,0 @@
-import $ from 'jquery';
-
-export default (tag = 'div', content = '', className = 'box') => {
-  const element = $(`<${tag}>`).addClass(className).html(content)
-  return element;
-};
--- homework_12/src/js/footer/footer.js
+++
@@ -1,9 +0,0 @@
-import  builder from '../elementBuilder';
-import './footer.scss';
-
-const content = '<p>Easycode 2017 (c)</p>';
-
-export const footer = builder(
-'footer', content, 'footer'
-);
-
