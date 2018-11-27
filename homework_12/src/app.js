6d9fb8b12f77c06c690ad3514cf5a150ce507797
dmgame <denis.m.pcspace@gmail.com>
2018-11-27 21:38:01 +0200

    classes

---
+++ homework_12/src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import  Header  from './components/header' ;
+import  Footer  from './components/footer' ;
+
+const Wrapper = () => {
+    <>
+        <Header />
+        <Footer />
+    </>
+}
+
+ReactDom.render(Wrapper, document.getElementById('app'));
+
+
---
+++ homework_12/src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+ <header className="header">
+  <strong><a href="">Logo</a></strong>
+ </header>
+)
+
+export default Header;
+
