0c09ddc6e6d86a378852194efcf969904e62c83c
OlgaLyakhvatskaya <belsquirrel213@gmail.com>
2018-12-17 00:40:41 +0200

    new master

--- homework_12/src/components/aside/Aside.js
+++
@@ -1,9 +0,0 @@
-import './aside.scss';
-
-const Aside = () => (
-  <aside className="aside">
-    <div className="item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
-  </aside>
-);
-
-export default Aside;
