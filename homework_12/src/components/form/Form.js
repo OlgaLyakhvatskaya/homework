8ff029b73d3cf669c59ad99642fc2bb4a297cbe3
OlgaLyakhvatskaya <belsquirrel213@gmail.com>
2018-12-18 00:08:25 +0200

    new v

--- homework_12/src/components/form/Form.js
+++ homework_12/src/components/form/Form.js
@@ -1,6 +1,25 @@
 import './form.scss';
 
 class UserForm extends Component {
+  static getDerivedStateFromProps({ data }, currentState){
+    if (!data) return null;
+
+    const props = Object.entries(data);
+    const isStateEmpty = props.every(([key]) => !currentState[key].value);
+
+    if (isStateEmpty) {
+      const state = {};
+
+      props.forEach(([key, value]) => state[key] = { value, error: '' })
+
+      return state;
+        // email: { value: data.email, error: '' },
+        // name:  { value: data.name, error: '' },
+        // user:  { value: data.user, error: '' }
+    }
+    return null;
+  }
+
   constructor(props) {
     super(props);
     this.fields = [
@@ -12,23 +31,79 @@ class UserForm extends Component {
     ];
     this.state = {};
 
-    this.fields.forEach(({ label }) => this.state[label] = '');
+    this.fields.forEach(({ label }) => this.state[label] = {
+      value: '',
+      error: ''
+    });
   }
 
-
   onChange = ({ target }) => {
+    const field = this.state[target.name]
+
     if (/checkbox|radio/i.test(target.type)) {
-      this.setState({ [target.name]: target.checked });
-    } else {
-      this.setState({ [target.name]: target.value });
+      this.setState({ [target.name]: {...field, value: target.checked} })
+    } else{
+      this.setState({ [target.name]: {...field, value: target.value} })
     }
   }
 
+  validate = ({ target }) => {
+    const field = this.fields.find(item => item.label === target.name);
+    const stateField = this.state[target.name];
+    let error = '';
+
+    if (target.value.lenght === 0 || !field.reg.test(target.value)) {
+      error = 'Is not valid';     
+    } else {
+      error = '';
+    }
+
+    if (target.name === 'repeatPassword' && this.state.password.value !== target.value) {
+      error = 'Passwords are not equled'
+      
+    }
+    this.setState({ [target.name]: { ...stateField, error } });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const data = {};
+
+    Object.entries(this.state).forEach(([key, { value }]) => data[key] = value)
+    if (this.props.onSave) {
+      this.props.onSave(data);
+    }
+    // console.log(data);
+  }
+
+  getButtonState = () => {
+    return Object.entries(this.state).some(([key, { error, value }]) => error || !value)
+  }
+
   render() {
-    const { state } = this;
-    return (
-      <form className="form">
-        { this.fields.map(field => <div className="wrapper"> <input type={field.secure ? 'password' : 'text'} name={field.label} placeholder={field.placeholder} value={state[field.label]} /></div>) }
+    const { state } = this
+    return (  
+      <form className="form" onSubmit={this.onSubmit}>
+        {
+          this.fields.map(field => 
+            <div className="wrapper">
+              <input  
+                type={field.secure ? 'password' : 'text'} 
+                name={field.label} 
+                value={state[field.label].value}
+                placeholder={field.placeholder}
+                onChange={this.onChange}
+                onBlur={this.validate}
+              />
+              {state[field.label].error && 
+              <mark>{state[field.label].error}</mark>}
+            </div>)
+        }
+        <input 
+          type="submit" 
+          disabled={this.getButtonState()}
+          value="Send" 
+        />
       </form>
     );
   }
--- homework_12/src/components/main/Main.js
+++ homework_12/src/components/main/Main.js
@@ -1,47 +1,18 @@
 import './main.scss';
-import Button from '../button';
-import ShowText from '../showText';
-import ListUserPost from '../ListUserPost';
-import GeoLocation from '../geoLocation';
-import Stopwatch from '../stopwatch';
+import Form from '../form';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: [],
-      posts: []
     };
-    this.getList();
-  }
-
-  getList = () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ users }));
-  }
-
-  getPost = (user) => {
-    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`)
-      .then(response => response.json())
-      .then(posts => this.setState({ posts }));
   }
 
   render() {
-    const { users, posts } = this.state;
-
     return (
       <main className="main">
         <div className="container">
-          <Button />
-          <ShowText />
-          <ListUserPost
-            list={users}
-            postList={posts}
-            click={this.getPost}
-          />
-          <GeoLocation />
-          <Stopwatch />
+          <Form />
         </div>
       </main>
     );
